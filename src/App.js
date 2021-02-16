8ba7819926a5af7fe47f67c1ccfa9df1fdb469bb
cabrera cristian <cabrecrieuro@gmail.com>
2021-02-15 22:20:42 -0300

    a ver gaston

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload cristian.
+          Edit <code>src/App.js</code> and save to reload cristian adrian.
         </p>
         <a
           className="App-link"
